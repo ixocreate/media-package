35cc40ae118e8f0343c119a4a28b3e9231f8140f
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-05-23 13:50:03 +0200

    added Processor

---
+++ src/Processor/ImageProcessor.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * kiwi-suite/media (https://github.com/kiwi-suite/media)
+ *
+ * @package kiwi-suite/media
+ * @see https://github.com/kiwi-suite/media
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+declare(strict_types=1);
+
+namespace KiwiSuite\Media\Processor;
+
+use Intervention\Image\ImageManager;
+use KiwiSuite\Media\MediaConfig;
+use Intervention\Image\Size;
+use Intervention\Image\Constraint;
+
+final class ImageProcessor
+{
+    private $imageParameters = [];
+
+    private $mediaConfig;
+
+    public function __construct(array $imageParameters, MediaConfig $mediaConfig)
+    {
+        $this->imageParameters = $imageParameters;
+        $this->mediaConfig = $mediaConfig;
+    }
+
+    public function process()
+    {
+        \extract($this->imageParameters);
+
+        $imageManager = new ImageManager(['driver' => $this->mediaConfig->getDriver()]);
+
+        if(!\is_dir($savingDir)) {
+            \mkdir($savingDir, 0777, true);
+        }
+
+        $image = $imageManager->make($path . $filename);
+        $imageWidth = $image->width();
+        $imageHeight = $image->height();
+
+        if ($imageWidth < $width && $imageHeight < $height) {
+            $image->resizeCanvas($width,$height);
+        }
+
+        if ($fit === true) {
+            $image->fit($width, $height, function(Constraint $constraint) {
+                $constraint->upsize();
+            });
+        } else {
+            $image->resize($width, $height, function(Constraint $constraint) use ($width, $height) {
+                if ($width === null || $height === null) {
+                    $constraint->aspectRatio();
+                    $constraint->upsize();
+                }
+            });
+        }
+
+        $image->save($savingDir . $filename);
+        $image->destroy();
+    }
+
+}
