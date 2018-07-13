d4ecc33845a3d9614e731bb37042d9a48e92d892
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-07-13 12:57:07 +0200

    fixed namespaces

--- bootstrap/servicemanager.php
+++ bootstrap/servicemanager.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 namespace KiwiSuite\Media;
 
 use KiwiSuite\ServiceManager\ServiceManagerConfigurator;
-use KiwiSuite\Media\MediaConfig;
-use KiwiSuite\Media\Factory\MediaConfigFactory;
+use KiwiSuite\Media\Config\MediaConfig;
+use KiwiSuite\Media\Config\Factory\MediaConfigFactory;
 use KiwiSuite\Media\Delegator\DelegatorSubManager;
 use KiwiSuite\Media\ImageDefinition\ImageDefinitionSubManager;
 
