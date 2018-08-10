b8b78163b3361c7e9166aa031c4d34278aa420f4
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-08-10 14:04:24 +0200

    included checkWhitelist & checkDuplicate

--- src/Action/UploadAction.php
+++ src/Action/UploadAction.php
@@ -11,6 +11,8 @@ declare(strict_types=1);
 
 namespace KiwiSuite\Media\Action;
 
+use KiwiSuite\Media\Config\MediaConfig;
+use KiwiSuite\Media\Config\MediaConfigurator;
 use KiwiSuite\Media\Delegator\DelegatorInterface;
 use KiwiSuite\Media\Entity\Media;
 use KiwiSuite\Media\Repository\MediaRepository;
@@ -39,20 +41,20 @@ final class UploadAction implements MiddlewareInterface
     private $delegatorSubManager;
 
     /**
-     * @var int
+     * @var MediaConfig
      */
-    private $countDelegators;
+    private $mediaConfig;
 
     /**
      * UploadAction constructor.
      * @param MediaRepository $mediaRepository
      * @param DelegatorSubManager $delegatorSubManager
      */
-    public function __construct(MediaRepository $mediaRepository, DelegatorSubManager $delegatorSubManager)
+    public function __construct(MediaRepository $mediaRepository, DelegatorSubManager $delegatorSubManager, MediaConfig $mediaConfig)
     {
         $this->mediaRepository = $mediaRepository;
         $this->delegatorSubManager = $delegatorSubManager;
-        $this->countDelegators = \count($this->delegatorSubManager->getServices());
+        $this->mediaConfig = $mediaConfig;
     }
 
     /**
@@ -69,16 +71,43 @@ final class UploadAction implements MiddlewareInterface
         }
 
         $upload = $request->getUploadedFiles()['file'];
+
         if (!($upload instanceof UploadedFile)) {
             return new ApiErrorResponse("invalid_file");
         }
 
-        do {
-            $basePath = \implode('/', str_split(bin2hex(random_bytes(3)), 2)) . '/';
-            $exists = \is_dir('data/media/' . $basePath);
-        } while ($exists === true);
+        if (!($this->checkWhitelist($upload))) {
+            return new ApiErrorResponse("file_type_not_supported");
+        }
 
-        \mkdir('data/media/' . $basePath, 0777, true);
+        if ($this->checkDuplicate($upload)) {
+            return new ApiErrorResponse('file_already_exists');
+        }
+
+        $media = $this->prepareMedia($upload);
+
+        foreach ($this->delegatorSubManager->getServiceManagerConfig()->getNamedServices() as $name => $delegator) {
+            /** @var DelegatorInterface $delegator */
+            $delegator = $this->delegatorSubManager->get($delegator);
+            if (!$delegator->isResponsible($media)) {
+                continue;
+            }
+            $delegator->process($media);
+        }
+
+        $this->mediaRepository->save($media);
+
+        return new ApiSuccessResponse();
+    }
+
+    /**
+     * @param UploadedFile $upload
+     * @return Media
+     * @throws \Exception
+     */
+    private function prepareMedia(UploadedFile $upload): Media
+    {
+        $basePath = $this->createDir();
         $filenameParts = \pathinfo($upload->getClientFilename());
         $slugify = new Slugify();
         $filename = $slugify->slugify($filenameParts['filename']) . '.' . $filenameParts['extension'];
@@ -93,25 +122,55 @@ final class UploadAction implements MiddlewareInterface
             'filename' => $filename,
             'mimeType' => finfo_file($finfo, 'data/media/' . $basePath . $filename),
             'size' => sprintf('%u', filesize('data/media/' . $basePath . $filename)),
+            'publicStatus' => false,
+            'hash' => hash_file('sha256','data/media/' . $basePath . $filename),
             'createdAt' => new \DateTimeImmutable(),
+            'updatedAt' => new \DateTimeImmutable(),
         ]);
+        return $media;
+    }
 
-        $notResponsible = 0;
-        foreach ($this->delegatorSubManager->getServiceManagerConfig()->getNamedServices() as $name => $delegator) {
-            /** @var DelegatorInterface $delegator */
-            $delegator = $this->delegatorSubManager->get($delegator);
-            if (!$delegator->isResponsible($media)) {
-                $notResponsible++;
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    private function createDir()
+    {
+        do {
+            $basePath = \implode('/', str_split(bin2hex(random_bytes(3)), 2)) . '/';
+            $exists = \is_dir('data/media/' . $basePath);
+        } while ($exists === true);
 
-                continue;
+        \mkdir('data/media/' . $basePath, 0777, true);
+
+        return $basePath;
+    }
+
+    /**
+     * @param $upload
+     * @return bool
+     */
+    private function checkDuplicate(UploadedFile $upload): bool
+    {
+        $count =
+            $this->mediaRepository->count(['hash' => hash_file('sha256',$upload->getStream()->getMetadata()['uri'])]);
+
+        return ($count > 0);
+    }
+
+    /**
+     * @param $upload
+     * @return bool
+     */
+    private function checkWhitelist(UploadedFile $upload): bool
+    {
+        $supported = false;
+        $filenameParts = \pathinfo($upload->getClientFilename());
+        foreach ($this->mediaConfig->whitelist() as $list) {
+            if (array_key_exists($filenameParts['extension'], $list) || in_array($upload->getClientMediaType(), $list)) {
+                $supported = true;
             }
-
-            $delegator->process($media);
         }
-        
-
-        $this->mediaRepository->save($media);
-
-        return new ApiSuccessResponse();
+        return $supported;
     }
 }
