984a1bf81911c938479d7a9d35d526c84b9bd293
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-08-14 15:58:37 +0200

    added publicStatus checkbox

--- src/Resource/MediaResource.php
+++ src/Resource/MediaResource.php
@@ -15,10 +15,18 @@ namespace KiwiSuite\Media\Resource;
 use KiwiSuite\Admin\Resource\DefaultAdminTrait;
 use KiwiSuite\Contract\Resource\AdminAwareInterface;
 use KiwiSuite\Contract\Schema\BuilderInterface;
+use KiwiSuite\Contract\Schema\ElementInterface;
 use KiwiSuite\Contract\Schema\Listing\ListSchemaInterface;
 use KiwiSuite\Contract\Schema\SchemaInterface;
 use KiwiSuite\Media\Action\IndexAction;
+use KiwiSuite\Media\Action\Media\DetailAction;
+use KiwiSuite\Media\Action\Media\UpdateAction;
+use KiwiSuite\Media\Entity\Media;
 use KiwiSuite\Media\Repository\MediaRepository;
+use KiwiSuite\Schema\Builder;
+use KiwiSuite\Schema\Elements\CheckboxElement;
+use KiwiSuite\Schema\Elements\ImageElement;
+use KiwiSuite\Schema\Listing\ListElement;
 use KiwiSuite\Schema\Listing\ListSchema;
 use KiwiSuite\Schema\Schema;
 
@@ -53,6 +61,14 @@ final class MediaResource implements AdminAwareInterface
     }
 
     /**
+     * @return null|string
+     */
+    public function detailAction(): ?string
+    {
+        return DetailAction::class;
+    }
+
+    /**
      * @param BuilderInterface $builder
      * @return SchemaInterface
      */
@@ -67,7 +83,20 @@ final class MediaResource implements AdminAwareInterface
      */
     public function updateSchema(BuilderInterface $builder): SchemaInterface
     {
-        return new Schema();
+        /** @var SchemaInterface $schema */
+        $schema = $builder->fromEntity(Media::class);
+        $element = $builder->create(CheckboxElement::class, 'publicStatus');
+        $element = $element->withLabel('Declare Public?');
+        $schema = $schema->withAddedElement($element);
+
+        $schema = $schema->remove('filename');
+        $schema = $schema->remove('hash');
+        $schema = $schema->remove('deletedAt');
+        $schema = $schema->remove('size');
+        $schema = $schema->remove('mimeType');
+        $schema = $schema->remove('basePath');
+
+        return $schema;
     }
 
     /**
