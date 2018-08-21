13879d334ec5a8532dee8d789250a20e9c124af9
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-08-21 11:46:47 +0200

    added action

--- src/Action/Media/ChangePublicStatusAction.php
+++ src/Action/Media/ChangePublicStatusAction.php
@@ -9,6 +9,7 @@ use KiwiSuite\Admin\Response\ApiSuccessResponse;
 use KiwiSuite\Media\Config\MediaConfig;
 use KiwiSuite\Media\Entity\Media;
 use KiwiSuite\Media\Repository\MediaRepository;
+use KiwiSuite\Media\Resource\MediaResource;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -17,47 +18,46 @@ use Psr\Http\Server\RequestHandlerInterface;
 final class ChangePublicStatusAction implements MiddlewareInterface
 {
     /**
-     * @var MediaConfig
-     */
-    private $mediaConfig;
-
-    /**
      * @var MediaRepository
      */
     private $mediaRepository;
+
+    /**
+     * @var MediaConfig
+     */
+    private $mediaConfig;
 
     /**
      * ChangePublicStatusAction constructor.
      * @param MediaConfig $mediaConfig
      * @param MediaRepository $mediaRepository
      */
-    public function __construct(MediaConfig $mediaConfig, MediaRepository $mediaRepository)
+    public function __construct(MediaRepository $mediaRepository, MediaConfig $mediaConfig)
     {
-        $this->mediaConfig = $mediaConfig;
         $this->mediaRepository = $mediaRepository;
+        $this->mediaConfig = $mediaConfig;
     }
     
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if (!$this->mediaConfig->publicStatus()) {
-            return new ApiErrorResponse();
-        }
         /** @var Media $media */
         $media = $this->mediaRepository->findOneBy(['id' => $request->getAttribute('id')]);
 
         if ($media === null) {
-            return new ApiErrorResponse();
+            return new ApiErrorResponse('given media Id does not exist');
+        }
+        $publicStatus = $request->getAttribute('publicStatus');
+        settype($publicStatus,'boolean');
+
+        if ($publicStatus === $media->publicStatus()) {
+            return new ApiErrorResponse('the publicStatus is already set to '.$publicStatus);
         }
 
-        if ($request->getAttribute('publicStatus') === $media->publicStatus()) {
-            return new ApiErrorResponse();
-        }
-
-        $media->with('publicStatus',$request->getAttribute('publicStatus'));
+        $media->with('publicStatus',$publicStatus);
         $media->with('updatedAt', new \DateTime());
 
         $this->mediaRepository->save($media);
 
-        return new ApiSuccessResponse();
+        return new ApiSuccessResponse('yay');
     }
 }
