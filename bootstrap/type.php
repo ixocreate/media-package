a88324f909a025163cdc892dbb2174490919ae06
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-08-23 15:37:20 +0200

    added types

--- bootstrap/type.php
+++ bootstrap/type.php
@@ -3,8 +3,18 @@ declare(strict_types=1);
 
 namespace KiwiSuite\Media;
 
+
+use KiwiSuite\Media\Type\ApplicationType;
+use KiwiSuite\Media\Type\AudioType;
+use KiwiSuite\Media\Type\ImageType;
+use KiwiSuite\Media\Type\MediaType;
+use KiwiSuite\Entity\Type\TypeConfigurator;
+use KiwiSuite\Media\Type\VideoType;
+
 /** @var TypeConfigurator $type */
 
-use KiwiSuite\Media\Type\ImageType;
-
 $type->addType(ImageType::class);
+$type->addType(MediaType::class);
+$type->addType(ApplicationType::class);
+$type->addType(VideoType::class);
+$type->addType(AudioType::class);
