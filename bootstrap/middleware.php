f3b5a13d44dd10b92b0c295c06e4b74c5d0d8177
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-08-23 15:36:56 +0200

    cleanup

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -5,7 +5,8 @@ use KiwiSuite\ApplicationHttp\Middleware\MiddlewareConfigurator;
 
 /** @var MiddlewareConfigurator $middleware */
 $middleware->addAction(\KiwiSuite\Media\Action\IndexAction::class);
-$middleware->addAction(\KiwiSuite\Media\Action\EditorAction::class);
+$middleware->addAction(\KiwiSuite\Media\Action\Image\EditorAction::class);
 $middleware->addAction(\KiwiSuite\Media\Action\Media\DetailAction::class);
 $middleware->addAction(\KiwiSuite\Media\Action\UploadAction::class);
 $middleware->addAction(\KiwiSuite\Media\Action\Media\ChangePublicStatusAction::class);
+$middleware->addAction(\KiwiSuite\Media\Action\Media\DeleteAction::class);
