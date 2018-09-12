c5b997868edbd99f1f6b99a03fabd4e98665fdda
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-09-12 16:13:35 +0200

    added FilterAction & DetailAction

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -10,3 +10,4 @@ $middleware->addAction(\KiwiSuite\Media\Action\Media\DetailAction::class);
 $middleware->addAction(\KiwiSuite\Media\Action\UploadAction::class);
 $middleware->addAction(\KiwiSuite\Media\Action\Media\ChangePublicStatusAction::class);
 $middleware->addAction(\KiwiSuite\Media\Action\Media\DeleteAction::class);
+$middleware->addAction(\KiwiSuite\Media\Action\Media\FilterAction::class);
