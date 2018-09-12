8f0cef8732168d819ada2944dae3a48c87e4fb59
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-09-12 16:13:51 +0200

    included FilterAction

--- bootstrap/pipe.php
+++ bootstrap/pipe.php
@@ -9,6 +9,7 @@ use KiwiSuite\ApplicationHttp\Pipe\GroupPipeConfigurator;
 use KiwiSuite\ApplicationHttp\Pipe\PipeConfigurator;
 use KiwiSuite\Media\Action\Media\ChangePublicStatusAction;
 use KiwiSuite\Media\Action\Media\DeleteAction;
+use KiwiSuite\Media\Action\Media\FilterAction;
 use KiwiSuite\Media\Action\UploadAction;
 
 $pipe->segmentPipe(AdminConfig::class)(function(PipeConfigurator $pipe) {
@@ -16,7 +17,8 @@ $pipe->segmentPipe(AdminConfig::class)(function(PipeConfigurator $pipe) {
 
         $pipe->group("admin.authorized")(function (GroupPipeConfigurator $group) {
             $group->post('/media/upload', UploadAction::class, 'admin.api.media.upload');
-            $group->patch('/media/public/{id}/{publicStatus}', ChangePublicStatusAction::class, 'admin.api.media.publicStatus');
+            $group->post('/media/filter', FilterAction::class, 'admin.api.media.filter');
+            $group->patch('/media/public/{id}', ChangePublicStatusAction::class, 'admin.api.media.publicStatus');
             $group->delete('/media/{id}', DeleteAction::class, 'admin.api.media.delete');
         });
     });
