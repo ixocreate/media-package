265e5d1bbd8664e109d2fcd103961ed47b9b2465
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-09-12 16:12:48 +0200

    added multiple application mimeTypes

--- bootstrap/media.php
+++ bootstrap/media.php
@@ -15,4 +15,33 @@ $media->setImageWhiteliste([
 $media->setTextWhitelist([
    'txt' =>  'text/plain'
 ]);
+$media->setApplicationWhitelist([
+    'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'doc' => 'application/msword',
+    'dot' => 'application/msword',
+    'dotx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+    'docm' => 'application/vnd.ms-word.document.macroEnabled.12',
+    'dotm' => 'application/vnd.ms-word.template.macroEnabled.12',
+    'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'xls' => 'application/vnd.ms-excel',
+    'xlt' => 'application/vnd.ms-excel',
+    'xla' => 'application/vnd.ms-excel',
+    'xltx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+    'xlsm' => 'application/vnd.ms-excel.sheet.macroEnabled.12',
+    'xltm' => 'application/vnd.ms-excel.template.macroEnabled.12',
+    'xlam' => 'application/vnd.ms-excel.addin.macroEnabled.12',
+    'xlsb' => 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+    'ppt' => 'application/vnd.ms-powerpoint',
+    'pot' => 'application/vnd.ms-powerpoint',
+    'pps' => 'application/vnd.ms-powerpoint',
+    'ppa' => 'application/vnd.ms-powerpoint',
+    'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    'potx' => 'application/vnd.openxmlformats-officedocument.presentationml.template',
+    'ppsx' => 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+    'ppam' => 'application/vnd.ms-powerpoint.addin.macroEnabled.12',
+    'pptm' => 'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+    'potm' => 'application/vnd.ms-powerpoint.template.macroEnabled.12',
+    'ppsm' => 'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
+    'pdf' => 'application/pdf',
+]);
 $media->setPublicStatus(false);
