f300b70feb575163d32ff63cee9675d8e83974cc
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-09-17 15:12:19 +0200

    update

--- src/Action/IndexAction.php
+++ src/Action/IndexAction.php
@@ -12,11 +12,13 @@ declare(strict_types=1);
 namespace KiwiSuite\Media\Action;
 
 use KiwiSuite\Admin\Response\ApiListResponse;
+use KiwiSuite\Media\Entity\Media;
 use KiwiSuite\Media\Uri\Uri;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Diactoros\Response\JsonResponse;
 
 final class IndexAction implements MiddlewareInterface
 {
@@ -25,6 +27,10 @@ final class IndexAction implements MiddlewareInterface
      */
     private $uri;
 
+    /**
+     * IndexAction constructor.
+     * @param Uri $uri
+     */
     public function __construct(Uri $uri)
     {
         $this->uri = $uri;
@@ -39,12 +45,11 @@ final class IndexAction implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $apiListResult = $handler->handle($request);
 
+        $apiListResult = $handler->handle($request);
         if (!($apiListResult instanceof ApiListResponse)) {
             return $apiListResult;
         }
-
         $items = $apiListResult->items();
         foreach ($items as $key => $value) {
             $items[$key]['thumb'] = $this->uri->generateImageUrl($value['basePath'], $value['filename'], 'admin-thumb');
