6e5ff2abba4ce8de9d4efa998febf0ecd7e17fe9
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-09-20 11:35:21 +0200

    added MediaCropRepository

--- bootstrap/repository.php
+++ bootstrap/repository.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace KiwiSuite\Media;
 
+use KiwiSuite\Media\Repository\MediaCropRepository;
 use KiwiSuite\Media\Repository\MediaRepository;
 
 /** @var \KiwiSuite\Database\Repository\RepositoryConfigurator $repository */
 $repository->addRepository(MediaRepository::class);
+$repository->addRepository(MediaCropRepository::class);
