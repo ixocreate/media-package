7c7297c78805eed29d001e868054832a845b9deb
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-11-12 11:01:41 +0100

    fixed exception namespace

---
+++ src/Exception/InvalidExtensionException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * kiwi-suite/media (https://github.com/kiwi-suite/media)
+ *
+ * @package kiwi-suite/media
+ * @see https://github.com/kiwi-suite/media
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+declare(strict_types=1);
+
+namespace KiwiSuite\Media\Exception;
+
+use Psr\Container\ContainerExceptionInterface;
+
+class InvalidExtensionException extends \RuntimeException implements
+    ContainerExceptionInterface
+{
+}
