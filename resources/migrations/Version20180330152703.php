23e7bd48a32b8cda2ee2b26f64c8e2b1f1c01999
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2018-12-28 11:40:12 +0100

    migration namespace

--- resources/migrations/Version20180330152703.php
+++ resources/migrations/Version20180330152703.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace KiwiMigration;
+namespace IxocreateMigration;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
--- resources/migrations/Version20180918092733.php
+++ resources/migrations/Version20180918092733.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace KiwiMigration;
+namespace IxocreateMigration;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
