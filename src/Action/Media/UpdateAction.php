5efe1d3e6137282488c838dd439e754a4e47315e
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-03-22 14:11:09 +0100

    refactored

--- src/Action/Media/UpdateAction.php
+++ src/Action/Media/UpdateAction.php
@@ -1,13 +1,20 @@
 <?php
+/**
+ * @see https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
 declare(strict_types=1);
 
 namespace Ixocreate\Media\Action\Media;
-
 
 use Ixocreate\Admin\Response\ApiErrorResponse;
 use Ixocreate\Admin\Response\ApiSuccessResponse;
 use Ixocreate\CommandBus\CommandBus;
 use Ixocreate\Media\Command\Media\UpdateCommand;
+use Ixocreate\Media\Entity\Media;
+use Ixocreate\Media\Repository\MediaRepository;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -19,25 +26,44 @@ final class UpdateAction implements MiddlewareInterface
      * @var CommandBus
      */
     private $commandBus;
+    /**
+     * @var MediaRepository
+     */
+    private $mediaRepository;
 
     /**
      * UpdateAction constructor.
+     * @param MediaRepository $mediaRepository
      * @param CommandBus $commandBus
      */
-    public function __construct(CommandBus $commandBus)
+    public function __construct(MediaRepository $mediaRepository,CommandBus $commandBus)
     {
         $this->commandBus = $commandBus;
+        $this->mediaRepository = $mediaRepository;
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        /** @var Media $media */
+        $media = $this->mediaRepository->findOneBy(['id' => $request->getAttribute('id')]);
+
+        if (empty($media)) {
+            return new ApiErrorResponse('given media Id does not exist');
+        }
+
         $data = $request->getParsedBody();
-        $data['id'] = $request->getAttribute('id');
+        $publicStatus = $data['publicStatus'];
+        $newFilename = $data['newFilename'];
 
-        $command = $this->commandBus->command(UpdateCommand::class, $data);
+        /** @var UpdateCommand $command */
+        $command = $this->commandBus->create(UpdateCommand::class, []);
+        $command = $command->withMedia($media);
+        $command = $command->withPublicStatus($publicStatus);
+        $command = $command->withNewFilename($newFilename);
+        $result = $this->commandBus->dispatch($command);
 
-        if (!$command->isSuccessful()) {
-            return new ApiErrorResponse('media-media-update', $command->messages());
+        if (!$result->isSuccessful()) {
+            return new ApiErrorResponse('media-media-update', $result->messages());
         }
 
         return new ApiSuccessResponse();
--- src/Command/Media/UpdateCommand.php
+++ src/Command/Media/UpdateCommand.php
@@ -1,13 +1,25 @@
 <?php
+/**
+ * @see https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
 declare(strict_types=1);
 
 namespace Ixocreate\Media\Command\Media;
 
 use Ixocreate\CommandBus\Command\AbstractCommand;
+use Ixocreate\Contract\Media\DelegatorInterface;
+use Ixocreate\Contract\Media\MediaInterface;
+use Ixocreate\Filesystem\Storage\StorageSubManager;
 use Ixocreate\Media\Config\MediaConfig;
-use Ixocreate\Media\ImageDefinition\ImageDefinitionSubManager;
+use Ixocreate\Media\Delegator\DelegatorSubManager;
+use Ixocreate\Media\Exception\InvalidConfigException;
+use Ixocreate\Media\MediaPaths;
 use Ixocreate\Media\Repository\MediaRepository;
 use Ixocreate\Media\Entity\Media;
+use League\Flysystem\FilesystemInterface;
 
 class UpdateCommand extends AbstractCommand
 {
@@ -22,55 +34,221 @@ class UpdateCommand extends AbstractCommand
     private $mediaConfig;
 
     /**
+     * @var DelegatorSubManager
+     */
+    private $delegatorSubManager;
+
+    /**
+     * @var StorageSubManager
+     */
+    private $storageSubManager;
+
+    /**
+     * @var FilesystemInterface
+     */
+    private $storage;
+
+    /**
+     * @var Media
+     */
+    private $media;
+
+    /**
+     * @var bool
+     */
+    private $publicStatus;
+
+    /**
+     * @var string
+     */
+    private $newFilename;
+
+    /**
      * UpdateCommand constructor.
+     * @param DelegatorSubManager $delegatorSubManager
+     * @param StorageSubManager $storageSubManager
      * @param MediaRepository $mediaRepository
      * @param MediaConfig $mediaConfig
      */
-    public function __construct(MediaRepository $mediaRepository, MediaConfig $mediaConfig, ImageDefinitionSubManager $imageDefinitionSubManager)
+    public function __construct(
+        DelegatorSubManager $delegatorSubManager,
+        StorageSubManager $storageSubManager,
+        MediaRepository $mediaRepository,
+        MediaConfig $mediaConfig
+    )
     {
         $this->mediaRepository = $mediaRepository;
         $this->mediaConfig = $mediaConfig;
-        foreach ($imageDefinitionSubManager->getServiceManagerConfig()->getNamedServices() as $name => $className) {
-            var_dump($className);
-            var_dump($name);
-        }
-        foreach ($imageDefinitionSubManager->getServices() as $name => $className) {
-            var_dump($className);
-            var_dump($name);
-        }
-        die('d');
+        $this->delegatorSubManager = $delegatorSubManager;
+        $this->storageSubManager = $storageSubManager;
     }
 
     /**
+     * @param Media $media
+     * @return UpdateCommand
+     */
+    public function withMedia(Media $media): UpdateCommand
+    {
+        $command = clone $this;
+        $command->media = $media;
+        return $command;
+    }
+
+    /**
+     * @param bool $publicStatus
+     * @return UpdateCommand
+     */
+    public function withPublicStatus(bool $publicStatus): UpdateCommand
+    {
+        $command = clone $this;
+        $command->publicStatus = $publicStatus;
+        return $command;
+    }
+
+    /**
+     * @param string $newFilename
+     * @return UpdateCommand
+     */
+    public function withNewFilename(string $newFilename): UpdateCommand
+    {
+        $command = clone $this;
+        $command->newFilename = $newFilename;
+        return $command;
+    }
+
+
+    /**
      * @return bool
+     * @throws \League\Flysystem\FileExistsException
+     * @throws \League\Flysystem\FileNotFoundException
+     * @throws \Exception
      */
     public function execute(): bool
     {
-        $data = $this->data();
-
-        /** @var Media $media */
-        $media = $this->mediaRepository->find($data['id']);
-
-        if ($media === null) {
-            return false;
+        if (!$this->storageSubManager->has('media')) {
+            throw new InvalidConfigException('Storage Config not set');
         }
 
-        if ($this->mediaConfig->publicStatus()) {
-            $desiredPublicStatus = $data['publicStatus'];
-            if ($media->publicStatus() !== $desiredPublicStatus) {
-                // TODO: DO SOMETHING
+        $this->storage = $this->storageSubManager->get('media');
+
+        // Filename
+        if ($this->newFilename !== null) {
+            $this->filterNewFilename();
+            /** @var string $newFilename */
+            $newFilename = $this->newFilename;
+
+            $fileInfo = \pathinfo($this->media->filename());
+
+            if ($fileInfo['filename'] !== $newFilename) {
+                $this->media = $this->renameFiles($this->media, $newFilename, $fileInfo);
+                $this->media = $this->media->with('filename', $this->newFilename . '.' . $fileInfo['extension']);
             }
         }
 
-        $newFilename = $data['newFilename'];
+        // PublicStatus
+        if ($this->publicStatus !== null) {
+            if ($this->mediaConfig->publicStatus()) {
+                $desiredPublicStatus = $this->publicStatus;
+                /**
+                 * check if already moved
+                 */
+                $publicDirectory = MediaPaths::PUBLIC_PATH . $this->media->basePath() . $this->media->filename();
+                $privateDirectory = MediaPaths::PRIVATE_PATH . $this->media->basePath() . $this->media->filename();
+                if ($desiredPublicStatus && !$this->storage->has($publicDirectory)) {
+                    $this->media = $this->moveMedia($this->media, MediaPaths::PRIVATE_PATH, MediaPaths::PUBLIC_PATH);
+                } elseif (!$desiredPublicStatus && !$this->storage->has($privateDirectory)) {
+                    $this->media = $this->moveMedia($this->media, MediaPaths::PUBLIC_PATH, MediaPaths::PRIVATE_PATH);
+                }
 
-        if ($media->filename() !== $newFilename) {
-            // TODO: DO SOMETHING
+                if ($this->media->publicStatus() !== $desiredPublicStatus) {
+                    $this->media = $this->media->with('publicStatus', $desiredPublicStatus);
+                }
+            }
+
         }
+
+        $this->media = $this->media->with('updatedAt', new \DateTimeImmutable());
+
+        $this->mediaRepository->save($this->media);
 
         return true;
     }
 
+    /**
+     * @param MediaInterface $media
+     * @param string $newFilename
+     * @param array $fileInfo
+     * @return MediaInterface
+     * @throws \League\Flysystem\FileExistsException
+     * @throws \League\Flysystem\FileNotFoundException
+     */
+    private function renameFiles(MediaInterface $media, string $newFilename, array $fileInfo): MediaInterface
+    {
+        $mediaPath = $media->publicStatus() ? MediaPaths::PUBLIC_PATH : MediaPaths::PRIVATE_PATH;
+        foreach ($this->delegatorSubManager->getServices() as $delegatorClassName) {
+            /** @var DelegatorInterface $delegator */
+            $delegator = $this->delegatorSubManager->get($delegatorClassName);
+            if ($delegator->isResponsible($media)) {
+                foreach ($delegator->directories() as $directory) {
+                    $this->storage->rename($mediaPath . $directory . $media->basePath() . $media->filename(), $mediaPath . $directory . $media->basePath() . $newFilename . '.' . $fileInfo['extension']);
+                }
+            }
+        }
+        $this->storage->rename($mediaPath . $media->basePath() . $media->filename(), $mediaPath . $media->basePath() . $newFilename . '.' . $fileInfo['extension']);
+
+        return $media;
+    }
+
+    /**
+     * @param MediaInterface $media
+     * @param string $fromMediaPath
+     * @param string $toMediaPath
+     * @return MediaInterface
+     * @throws \League\Flysystem\FileExistsException
+     * @throws \League\Flysystem\FileNotFoundException
+     */
+    private function moveMedia(MediaInterface $media, string $fromMediaPath, string $toMediaPath): MediaInterface
+    {
+        /**
+         * move source file
+         */
+        $this->storage->rename($fromMediaPath . $media->basePath() . $media->filename(), $toMediaPath . $media->basePath() . $media->filename());
+
+        /**
+         * move output files from delegators as well
+         */
+        foreach ($this->delegatorSubManager->getServices() as $delegatorClassName) {
+            /** @var DelegatorInterface $delegator */
+            $delegator = $this->delegatorSubManager->get($delegatorClassName);
+
+            if (!$delegator->isResponsible($media)) {
+                continue;
+            }
+
+            foreach ($delegator->directories() as $directory) {
+                $this->storage->rename($fromMediaPath . $directory . $media->basePath() . $media->filename(), $toMediaPath . $directory . $media->basePath() . $media->filename());
+            }
+        }
+
+        return $media;
+    }
+
+
+    private function filterNewFilename(): void
+    {
+        // remove whitespace
+        $newFilename = trim($this->newFilename);
+        // remove Extension
+        $newFilename = \pathInfo($newFilename, PATHINFO_FILENAME);
+        // remove all special Characters except "-_ /."
+        $newFilename = \preg_replace("/([^A-Za-z0-9ÖöÄäÜü\/\-_\.])/", "", $newFilename);
+
+        $this->newFilename = $newFilename;
+    }
+
+    /**
+     * @return string
+     */
     public static function serviceName(): string
     {
         return 'media-media-update';
