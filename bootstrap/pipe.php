8aee883794879b6913c23fbaa75ff0159bb44f2e
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-03-22 14:41:15 +0100

    cleanup

--- bootstrap/pipe.php
+++ bootstrap/pipe.php
@@ -9,19 +9,12 @@ use Ixocreate\Admin\Config\AdminConfig;
 use Ixocreate\ApplicationHttp\Pipe\GroupPipeConfigurator;
 use Ixocreate\ApplicationHttp\Pipe\PipeConfigurator;
 use Ixocreate\Media\Action\Image\EditorAction;
-use Ixocreate\Media\Action\Image\ImageDefinitionDetailAction;
-use Ixocreate\Media\Action\Image\ImageDefinitionListAction;
-use Ixocreate\Media\Action\Media\ChangePublicStatusAction;
 use Ixocreate\Media\Action\Media\DeleteAction;
 use Ixocreate\Media\Action\Media\DetailAction;
-use Ixocreate\Media\Action\Media\EditAction;
-use Ixocreate\Media\Action\Media\FilterAction;
 use Ixocreate\Media\Action\Media\IndexAction;
-use Ixocreate\Media\Action\Media\PrivateStreamAction;
 use Ixocreate\Media\Action\Media\UpdateAction;
 use Ixocreate\Media\Action\StreamAction;
 use Ixocreate\Media\Action\UploadAction;
-use Ixocreate\Media\Middleware\StreamMiddleware;
 
 $pipe->get('/media/stream/{token}', StreamAction::class, 'media.stream');
 
