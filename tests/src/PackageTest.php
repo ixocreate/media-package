f85b216d26576454af520956e7f0cd7628092f7c
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-04-26 15:00:51 +0200

    add package test

---
+++ tests/src/PackageTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Test\Media;
+
+use Ixocreate\Application\Configurator\ConfiguratorRegistryInterface;
+use Ixocreate\Application\Service\ServiceRegistryInterface;
+use Ixocreate\Media\Handler\MediaHandlerBootstrapItem;
+use Ixocreate\Media\ImageDefinition\ImageDefinitionBootstrapItem;
+use Ixocreate\Media\MediaBootstrapItem;
+use Ixocreate\Media\Package;
+use Ixocreate\ServiceManager\ServiceManagerInterface;
+use PHPUnit\Framework\TestCase;
+
+class PackageTest extends TestCase
+{
+    /**
+     * @covers \Ixocreate\Media\Package
+     */
+    public function testPackage()
+    {
+        $configuratorRegistry = $this->getMockBuilder(ConfiguratorRegistryInterface::class)->getMock();
+        $serviceRegistry = $this->getMockBuilder(ServiceRegistryInterface::class)->getMock();
+        $serviceManager = $this->getMockBuilder(ServiceManagerInterface::class)->getMock();
+
+        $package = new Package();
+        $package->configure($configuratorRegistry);
+        $package->addServices($serviceRegistry);
+        $package->boot($serviceManager);
+
+        $this->assertSame([
+            MediaBootstrapItem::class,
+            MediaHandlerBootstrapItem::class,
+            ImageDefinitionBootstrapItem::class,
+        ], $package->getBootstrapItems());
+        $this->assertDirectoryExists($package->getConfigDirectory());
+        $this->assertNull($package->getConfigProvider());
+        $this->assertNull($package->getDependencies());
+        $this->assertDirectoryExists($package->getBootstrapDirectory());
+    }
+}
