079e417952ec57bab21845c5353181b4481536b3
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-05-31 13:53:25 +0200

    added Template\MediaExtension

--- src/Template/MediaExtension.php
+++ src/Template/MediaExtension.php
@@ -10,7 +10,12 @@ declare(strict_types=1);
 namespace Ixocreate\Media\Template;
 
 use Ixocreate\Media\Config\MediaConfig;
+use Ixocreate\Media\Entity\MediaDefinitionInfo;
+use Ixocreate\Media\ImageDefinition\ImageDefinitionSubManager;
+use Ixocreate\Media\MediaInterface;
 use Ixocreate\Media\Repository\MediaDefinitionInfoRepository;
+use Ixocreate\Media\Repository\MediaRepository;
+use Ixocreate\Media\Schema\Type\ImageType;
 use Ixocreate\Template\Extension\ExtensionInterface;
 
 final class MediaExtension implements ExtensionInterface
@@ -22,19 +27,38 @@ final class MediaExtension implements ExtensionInterface
     private $mediaConfig;
 
     /**
+     * @var MediaRepository
+     */
+    private $mediaRepository;
+
+    /**
      * @var MediaDefinitionInfoRepository
      */
     private $mediaDefinitionInfoRepository;
 
     /**
+     * @var ImageDefinitionSubManager
+     */
+    private $imageDefinitionSubManager;
+
+    /**
      * MediaExtension constructor.
      * @param MediaConfig $mediaConfig
+     * @param MediaRepository $mediaRepository
      * @param MediaDefinitionInfoRepository $mediaDefinitionInfoRepository
+     * @param ImageDefinitionSubManager $imageDefinitionSubManager
      */
-    public function __construct(MediaConfig $mediaConfig, MediaDefinitionInfoRepository $mediaDefinitionInfoRepository)
+    public function __construct(
+        MediaConfig $mediaConfig,
+        MediaRepository $mediaRepository,
+        MediaDefinitionInfoRepository $mediaDefinitionInfoRepository,
+        ImageDefinitionSubManager $imageDefinitionSubManager
+    )
     {
         $this->mediaConfig = $mediaConfig;
         $this->mediaDefinitionInfoRepository = $mediaDefinitionInfoRepository;
+        $this->mediaRepository = $mediaRepository;
+        $this->imageDefinitionSubManager = $imageDefinitionSubManager;
     }
 
     /**
@@ -45,10 +69,73 @@ final class MediaExtension implements ExtensionInterface
         return 'media';
     }
 
+    /**
+     * @return $this
+     */
     public function __invoke()
     {
         return $this;
     }
 
+    /**
+     * @param ImageType $imageType
+     * @return mixed
+     */
+    public function width(ImageType $imageType)
+    {
+        /** @var MediaInterface $media */
+        $media = $imageType->value();
 
+        return $media->metaData()['width'];
+    }
+
+    /**
+     * @param ImageType $imageType
+     * @return mixed
+     */
+    public function height(ImageType $imageType)
+    {
+        /** @var MediaInterface $media */
+        $media = $imageType->value();
+
+        return $media->metaData()['height'];
+    }
+
+    /**
+     * @param ImageType $imageType
+     * @param string $imageDefinitionServiceName
+     * @return int
+     */
+    public function definitionWidth(ImageType $imageType, string $imageDefinitionServiceName)
+    {
+        $media = $imageType->value();
+
+        if ($this->imageDefinitionSubManager->has($imageDefinitionServiceName)) {
+            /** @var MediaDefinitionInfo $result */
+            $result = $this->mediaDefinitionInfoRepository->findBy(
+                ['mediaId' => $media->id(), 'imageDefinition' => $imageDefinitionServiceName]
+            )[0];
+
+            return $result->width();
+        }
+    }
+
+    /**
+     * @param ImageType $imageType
+     * @param string $imageDefinitionServiceName
+     * @return int
+     */
+    public function definitionHeight(ImageType $imageType, string $imageDefinitionServiceName)
+    {
+        $media = $imageType->value();
+
+        if ($this->imageDefinitionSubManager->has($imageDefinitionServiceName)) {
+            /** @var MediaDefinitionInfo $result */
+            $result = $this->mediaDefinitionInfoRepository->findBy(
+                ['mediaId' => $media->id(), 'imageDefinition' => $imageDefinitionServiceName]
+            )[0];
+
+            return $result->height();
+        }
+    }
 }
