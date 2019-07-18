8ac4a3a8def4171a27f025e29ada595e16f8d8e9
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2019-07-18 10:04:35 +0200

    add AdminThumb image definition

---
+++ bootstrap/image-definition.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOLIT GmbH
+ * @license MIT License
+ */
+declare(strict_types=1);
+
+namespace Ixocreate\Media;
+
+use Ixocreate\Media\ImageDefinition\AdminThumb;
+use Ixocreate\Media\ImageDefinition\ImageDefinitionConfigurator;
+
+/** @var ImageDefinitionConfigurator $imageDefinition */
+$imageDefinition->addImageDefinition(AdminThumb::class);
---
+++ src/ImageDefinition/AdminThumb.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOLIT GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Media\ImageDefinition;
+
+use Ixocreate\Media\ImageDefinitionInterface;
+
+final class AdminThumb implements ImageDefinitionInterface
+{
+    /**
+     * @return string
+     */
+    public static function serviceName(): string
+    {
+        return 'admin-thumb';
+    }
+
+    /**
+     * @return int|null
+     */
+    public function width(): ?int
+    {
+        return 500;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function height(): ?int
+    {
+        return 500;
+    }
+
+    public function upscale(): bool
+    {
+        return false;
+    }
+
+    public function mode(): string
+    {
+        return ImageDefinitionInterface::MODE_CANVAS;
+    }
+
+    /**
+     * @return string
+     */
+    public function directory(): string
+    {
+        return 'admin-thumb';
+    }
+}
