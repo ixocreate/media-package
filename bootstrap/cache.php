60502415c7d0b2a1fbfbcce615c215654ad081e7
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-11-08 12:17:51 +0100

    removed class

--- bootstrap/cache.php
+++ bootstrap/cache.php
@@ -12,12 +12,10 @@ namespace Ixocreate\Cms\Cache;
 use Ixocreate\Cache\CacheConfigurator;
 use Ixocreate\Cache\Option\InMemory;
 use Ixocreate\Media\Cacheable\MediaCacheable;
-use Ixocreate\Media\Cacheable\MediaDefinitionInfoCacheable;
 use Ixocreate\Media\Cacheable\UrlVariantCacheable;
 
 /** @var CacheConfigurator $cache */
 $cache->addCacheable(UrlVariantCacheable::class);
-$cache->addCacheable(MediaDefinitionInfoCacheable::class);
 $cache->addCacheable(MediaCacheable::class);
 
 $cache->addCache('media', new InMemory());
